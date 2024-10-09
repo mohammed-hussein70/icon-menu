0f3564b40b22f409182018a74b0c63e47f0cbe65
mohammed <mohammedhussein004@gmail.com>
2024-10-09 09:50:37 +0300

    Add files icon menu

---
+++ main.js
@@ -0,0 +1,6 @@
+let cont = document.querySelector(".container1");
+let bar_2 = document.querySelectorAll(".bar");
+
+cont.addEventListener("click", function () {
+  this.classList.toggle("change");
+});
